10a6a459aa67745ea7e26ac8f072476663d184ac
SeekEquilibrium <markovic.ra107.2018@uns.ac.rs>
2021-11-24 08:38:26 +0100

    feat(medicalRecord): Finished working on integration test

--- Hospital/HospitalIntegrationTests/Startup.cs
+++ Hospital/HospitalIntegrationTests/Startup.cs
@@ -5,12 +5,17 @@ using HospitalAPI.ImplRepository;
 using HospitalAPI.ImplService;
 using HospitalAPI.Repository;
 using HospitalAPI.Validation;
+using HospitalLibrary.MedicalRecords.Model;
+using HospitalLibrary.MedicalRecords.Model.Enums;
 using HospitalLibrary.MedicalRecords.Repository.Repository.Interface;
+using HospitalLibrary.Model.Enumeration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using static HospitalAPI.Mapper.Mapper;
 
@@ -62,7 +67,56 @@ namespace HospitalIntegrationTests
             {
                 options.UseInMemoryDatabase("InMemoryDbForTesting");
             });
+            var serviceProvider = services.BuildServiceProvider();
 
+            using (var scope = serviceProvider.CreateScope())
+            {
+                using (var db = scope.ServiceProvider.GetRequiredService<MyDbContext>())
+                    try
+                    {
+
+
+                        db.Database.EnsureCreated();
+                        InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+            }
+        }
+
+
+        public static void InitializeDbForTests(MyDbContext db)
+        {
+            CreatePatients();
+            db.SaveChanges();
+        }
+
+        public static List<Patient> CreatePatients()
+        {
+            List<Patient> listOfPatients = new List<Patient>();
+            Doctor doctor = new Doctor();
+            doctor.Id = "1";
+            List<Allergy> allergies = new List<Allergy>();
+
+            Patient newPatientA1 = new Patient("2", "Slavko", "Vranjes", DateTime.Now,
+                "054236971333", "Partizanskih baza 8.", "0666423699", "slavko@gmail.com",
+                "slavko", "slavko123", Gender.male,
+                "Novi Sad", "Serbia", UserType.patient, BloodType.B, RhFactor.positive,
+                189, 85, doctor, allergies);
+
+
+            Patient newPatientB2 = new Patient("3", "Marko", "Markovic", DateTime.Now,
+                "0542369712546", "Partizanskih baza 7.", "0666423599", "marko@gmail.com",
+                "SeekEquilibrium", "mira123", Gender.female,
+                "Novi Sad", "Serbia", UserType.patient, BloodType.A, RhFactor.negative,
+                180, 85, doctor, allergies);
+
+            listOfPatients.Add(newPatientA1);
+            listOfPatients.Add(newPatientB2);
+
+            return listOfPatients;
         }
 
         public void Configure(IApplicationBuilder app)
@@ -82,5 +136,5 @@ namespace HospitalIntegrationTests
             });
         }
     }
-    
+
 }
