306a5c88c7a7dc5ec4b97031a580ad0092a9e4f5
SeekEquilibrium <markovic.ra107.2018@uns.ac.rs>
2021-12-06 10:12:39 +0100

    feat(viewAppoint/cancelAppoint): Added integration tests for view and cancel appointments

---
+++ Hospital/HospitalIntegrationTests/ViewAppointmentsTest.cs
@@ -0,0 +1,83 @@
+﻿using HospitalLibrary.MedicalRecords.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HospitalIntegrationTests
+{
+    
+    public class ViewAppointmentsTest : IClassFixture<InjectionFixture>
+    {
+        private readonly InjectionFixture injection;
+
+        public ViewAppointmentsTest(InjectionFixture injection)
+        {
+            this.injection = injection;
+        }
+
+        [Fact]
+        public async Task Get_All_Patient_AppointmentsAsync()
+        {
+            var url = "api/appointment";
+
+            var response = await injection.Client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            var resultString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Appointment>>(resultString);
+
+            // Assert.Equal(0, result[0].Doctorid);         
+            // Assert.Equal(0, result[1].Doctorid);
+        }
+
+        [Fact]
+        public async Task Get_All_Patient_Completed_AppointmentsAsync()
+        {
+            var url = "api/appointment/completed";
+
+            var response = await injection.Client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            var resultString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Appointment>>(resultString);
+
+            // Assert.Equal(0, result[0].Doctorid);         
+            // Assert.Equal(0, result[1].Doctorid);
+        }
+        [Fact]
+        public async Task Get_All_Patient_Pending_AppointmentsAsync()
+        {
+            var url = "api/appointment/pending";
+
+            var response = await injection.Client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            var resultString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Appointment>>(resultString);
+
+            // Assert.Equal(0, result[0].Doctorid);         
+            // Assert.Equal(0, result[1].Doctorid);
+        }
+        [Fact]
+        public async Task Get_All_Patient_Cancelled_AppointmentsAsync()
+        {
+            var url = "api/appointment/cancelled";
+
+            var response = await injection.Client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            var resultString = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<Appointment>>(resultString);
+
+            // Assert.Equal(0, result[0].Doctorid);         
+            // Assert.Equal(0, result[1].Doctorid);
+        }
+    }
+}
