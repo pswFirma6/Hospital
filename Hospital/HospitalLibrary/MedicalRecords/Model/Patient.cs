98198fdaa05268b6702b629b382947e2ad69e522
simin161 <simin.ra161.2018@uns.ac.rs>
2021-12-14 00:33:42 +0100

    fix: docker compose

--- Hospital/HospitalLibrary/MedicalRecords/Model/Patient.cs
+++ Hospital/HospitalLibrary/MedicalRecords/Model/Patient.cs
@@ -12,7 +12,7 @@ namespace HospitalLibrary.MedicalRecords.Model
         public int Height { get; set; }
         public int Weight { get; set; }
         public virtual ICollection<Allergy> Allergies { get; set; }
-        public string DoctorId { get; set; }
+        public int DoctorId { get; set; }
         public virtual Doctor Doctor { get; set; }
         public bool Blocked { get; set; }
         public bool Malicious { get; set; }
